0340da8aaf801cbde5a6f4c74a852a11d276db6a
almontao <33377248+almontao@users.noreply.github.com>
2025-07-13 22:49:58 -0500

    Update spanish text in base page

--- js/translations.js
+++ js/translations.js
@@ -100,24 +100,24 @@ const translations = {
     "portfolio-4-category": "Proyectos",
 
     "resources": "Recursos",
-    "resource-subtitle-1": "Artículos y presentaciones",  // Papers and slides
-    "resource-1-type": "Publicaciones",  //Publications
-    "resource-1-name": "Producción académica",  //Academic Productions
-    "resource-1-button": "Ver listado",  //View list
-    "resource-1-description": "Artículos y capítulos de libros.",  //Papers and book chapters.
-    "resource-2-type": "Presentaciones",  //Slides
-    "resource-2-name": "Recursos adicionales",  //Additional Resources
-    "resource-2-button": "Ver listado",  //View list
-    "resource-2-description": "Material usado en eventos.",  //Materials used in events.
-    "resource-subtitle-2": "Trabajos de grado",  // Academic thesis
-    "resource-3-type": "Pregrado",  //Undergraduate
-    "resource-3-name": "Lingüística",  //Linguistics
-    "resource-3-button": "Ver en repositorio",  //View in repository
-    "resource-3-desription": "Electrofisiología cerebral y fijaciones oculares durante el proceso de producción articulatoria de los rasgos vocálicos del español",  //Cerebral electrophysiology and ocular fixations during the articulatory production process of the vowel features of Spanish
-    "resource-4-type": "Postgrado",  //Postgraduate
-    "resource-4-name": "Neurociencias",  //Neurosciences
-    "resource-4-button": "Sin publicar",  //Unpublished
-    "resource-4-description": "Temporalidad lingüística y aprendizaje supervisado: clasificación de los campos léxicos de pasado, presente y futuro mediante el procesamiento de imágenes de resonancia magnética funcional (IRMf) y estímulos narrativos de lenguaje natural",  //Linguistic temporality and supervised learning: classification of the lexical fields of past, present and future through Functional Magnetic Resonance Imaging (fMRI) processing and narrative natural language stimuli
+    "resource-subtitle-1": "Artículos y presentaciones",
+    "resource-1-type": "Publicaciones",
+    "resource-1-name": "Producción académica",
+    "resource-1-button": "Ver listado",
+    "resource-1-description": "Artículos y capítulos de libros.",
+    "resource-2-type": "Presentaciones",
+    "resource-2-name": "Recursos adicionales",
+    "resource-2-button": "Ver listado",
+    "resource-2-description": "Material usado en eventos.",
+    "resource-subtitle-2": "Trabajos de grado",
+    "resource-3-type": "Pregrado",
+    "resource-3-name": "Lingüística",
+    "resource-3-button": "Ver en repositorio",
+    "resource-3-desription": "Electrofisiología cerebral y fijaciones oculares durante el proceso de producción articulatoria de los rasgos vocálicos del español",
+    "resource-4-type": "Postgrado",
+    "resource-4-name": "Neurociencias",
+    "resource-4-button": "Sin publicar",
+    "resource-4-description": "Temporalidad lingüística y aprendizaje supervisado: clasificación de los campos léxicos de pasado, presente y futuro mediante el procesamiento de imágenes de resonancia magnética funcional (IRMf) y estímulos narrativos de lenguaje natural",
 
     "contact": "Contacto",
     "email": "Correo electrónico",
